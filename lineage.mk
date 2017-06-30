44215c1611f31fcd46f2f9243377220068e53b33
acoffeerunner <acoffeerunner@gmail.com>
2017-06-30 12:28:36 +0000

    Product mk: Changes for Lineage
    Rebranding from CM to LOS.
    
    Change-Id: I62711dc3b23231a0b8d290bf8e63211c7d3f77e6

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/n8000/full_n8000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8000
-PRODUCT_NAME := cm_n8000
+PRODUCT_NAME := lineage_n8000
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N8000
 PRODUCT_MANUFACTURER := samsung
